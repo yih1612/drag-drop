b36035842dad66a8e3f0bb45ee3993113096a1ad
yih1612 <yih1612@naver.com>
2025-06-18 07:50:24 +0900

    122. DOM 요소 선택 및 OOP 렌더링

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-// Code goes here!
+class ProjectInput {
+  // tsconfig.js의 lib의 'dom'이 있기 때문에 html 요소 타입들을 사용할 수 있다.
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    // importNode(1, 2)
+    // 1. template 태그 안에 있는 html 코드에 대한 참조 제공.
+    // 2. 노드의 자식들을 포함해서 가져온다.(깊은 복사)
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    // insertAdjacentElement(1, 2): html 요소를 삽입할 때 사용.
+    // 1: 삽입할 곳.
+    // 2: 삽입할 요소.
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
